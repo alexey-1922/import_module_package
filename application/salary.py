b23198a5ba0cabd77e061105d6fbe08c6416b75c
Alexey Alyabiev <alexalabev@yandex.ru>
2023-07-08 17:17:46 +0300

    hw

---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary():
+    print('Information for calculate_salary')
---
+++ stat_text.py
@@ -0,0 +1,6 @@
+from TextStatistic import count_unique_words
+
+
+filename = r"README.md"
+cuw = count_unique_words(filename)
+print(cuw)
